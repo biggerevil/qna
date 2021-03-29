11bcf2e093e3ab56f45bd4d7652df2a30b6f7208
biggerevil <tugunrus@icloud.com>
2021-03-30 01:42:52 +0300

    Generating and writing tests for Answer

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:answer) { create(:answer) }
+  let(:question) { create(:question) }
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer to database' do
+        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to question show view' do
+        post :create, params: { question_id: question.id, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer, :invalid) } }.to change(Answer, :count).by(0)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question.id, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
