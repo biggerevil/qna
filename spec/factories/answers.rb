d48ede3e1ba9308572379e0b72708dc4598c9c6c
biggerevil <tugunrus@icloud.com>
2021-04-04 01:31:05 +0300

    Upgrade factories

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  sequence :answer_body do |n|
+    "Answer ##{n}"
+  end
+
   factory :answer do
-    body { 'MyString' }
+    body { generate(:answer_body) }
     question { nil }
+    author { association :user }
 
     trait :invalid do
       body { nil }
