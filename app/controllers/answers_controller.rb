6c5b3b645261f935520c78747ff5a01eac180c99
biggerevil <tugunrus@icloud.com>
2021-04-04 01:32:56 +0300

    Answer controller and views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,17 +1,33 @@
 # frozen_string_literal: true
 
 class AnswersController < ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
+
   expose :answer
+  expose :question, -> { Question.find(params[:question_id]) }
 
   def create
-    question = Question.find(params[:question_id])
-    question.answers.push(answer)
+    current_user.answers.push(answer)
+    answer.question = question
 
+    puts "question.answer before save = #{question.answers.inspect}"
     if answer.save
-      redirect_to question
+      puts "question.answer after save = #{question.answers.inspect}"
+      redirect_to question, notice: 'Your answer was successfully created.'
     else
-      render :new
+      render 'questions/show'
     end
+  end
+
+  def destroy
+    unless current_user.author_of?(answer)
+      redirect_to question_path(answer.question), notice: 'You are not author of this answer!'
+      return
+    end
+    answer.delete
+
+    question = answer.question
+    redirect_to question_path(question)
   end
 
   private
