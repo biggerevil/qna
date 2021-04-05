4fa85c24f0a291f4d7a6f60e0b238536ec22d789
biggerevil <tugunrus@icloud.com>
2021-04-05 14:26:20 +0300

    User spec changes
    1. Rename describe
    2. Remove contexts as long as they have only one test inside

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,22 +8,17 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_many(:answers).dependent(:destroy) }
   it { is_expected.to have_many(:questions).dependent(:destroy) }
 
-  describe 'Testing author_of? method' do
+  describe '#author_of?' do
     let(:user) { create(:user) }
     let(:question) { create(:question, author: user) }
+    let(:second_user) { create(:user) }
 
-    context 'Author' do
-      it 'Returns true' do
-        expect(user).to be_author_of(question)
-      end
+    it 'Returns true for author' do
+      expect(user).to be_author_of(question)
     end
 
-    context 'Not author' do
-      let(:second_user) { create(:user) }
-
-      it 'Returns false' do
-        expect(second_user).not_to be_author_of(question)
-      end
+    it 'Returns false for not author' do
+      expect(second_user).not_to be_author_of(question)
     end
   end
 end
